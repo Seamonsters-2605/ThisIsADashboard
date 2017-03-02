4141ad6fb35b8f2288d8c527e2d89f9e77731319
vanjac <vanjac@users.noreply.github.com>
2017-03-02 13:12:00 -0800

    Fix typo

--- thisIsTheDashboardCode.py
+++ thisIsTheDashboardCode.py
@@ -138,7 +138,7 @@ class ThisIsTheDashboardApp:
         canvas = Canvas(frame)
 
         canvas.create_line(30,100,30,200)
-        canvas.create_line(30,200,70,200)ss
+        canvas.create_line(30,200,70,200)
         canvas.create_line(150,100,150,200)
         canvas.create_line(150,200,190,200)
 
