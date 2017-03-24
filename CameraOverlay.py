ffc5821baa1f34c69e44adfd158e5b39a748ac84
Jae-Chan Lee <ljae0724@gmail.com>
2017-03-23 19:26:03 -0700

    Added static values

--- CameraOverlay.py
+++ CameraOverlay.py
@@ -5,7 +5,6 @@ app = Application()
 
 #app.Connect(path = r"C:\Program Files\Mozilla Firefox\firefox.exe")
 app.Start(r"C:\Program Files\Mozilla Firefox\firefox.exe http://10.26.5.2:1187")
-#kaz is lame from Jae-chan
 win = app.window()
 
 wrap = win.WrapperObject()
@@ -24,14 +23,16 @@ master.resizable(width=False, height=False)
 
 x=0
 y=0
-#w.create_line(240+x,150+y,240+x,150+y,width=3,fill="deep pink")
-#w.create_line(240+x,105+y,225+x,85+y,width=3,fill="deep pink")
-#w.create_line(240+x,142+y,290+x,132+y,width=3,fill="deep pink")
-#w.create_line(290+x,132+y,287+x,103+y,width=3,fill="deep pink")
-#w.create_line(287+x,103+y,300+x,80+y,width=3,fill="deep pink")
-#w.create_line(232+x,360+y,232+x,266+y,width=3,fill="deep pink")
-#w.create_line(174+x,313+y,290+x,313+y,width=3,fill="deep pink")
-w.create_line(250+x,195+y,400+x,195+y,width=6,fill="deep pink")
-w.create_line(250+x,237+y,400+x,237+y,width=6,fill="deep pink")
-w.create_line(325+x,195+y,325+x,237+y,width=6,fill="deep pink")
+Color = "deep pink"
+Width = 6
+#w.create_line(240+x,150+y,240+x,150+y,width=Width,fill="Color")
+#w.create_line(240+x,105+y,225+x,85+y,width=Width,fill="Color")
+#w.create_line(240+x,142+y,290+x,132+y,width=Width,fill="Color")
+#w.create_line(290+x,132+y,287+x,103+y,width=Width,fill="Color")
+#w.create_line(287+x,103+y,300+x,80+y,width=Width,fill="Color")
+#w.create_line(232+x,360+y,232+x,266+y,width=Width,fill="Color")
+#w.create_line(174+x,313+y,290+x,313+y,width=Width,fill="Color")
+w.create_line(250+x,195+y,400+x,195+y,width=Width,fill="Color")
+w.create_line(250+x,237+y,400+x,237+y,width=Width,fill="Color")
+w.create_line(325+x,195+y,325+x,237+y,width=Width,fill="Color")
 mainloop()
